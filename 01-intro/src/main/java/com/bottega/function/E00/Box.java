b9f2886e594eab1604ba196a28600337c0452b5b
Maciej Zubala <maciej.zubala@gmail.com>
2024-05-13 11:22:42 +0200

    box fix

--- 01-intro/src/main/java/com/bottega/function/E00/Box.java
+++ 01-intro/src/main/java/com/bottega/function/E00/Box.java
@@ -3,9 +3,9 @@ package com.bottega.function.E00;
 /**
  * Create a generic box class that allows to hold single element of any type.
  * Define following operations on the box class:
- * 1. factory method creating empty box
+ * 1. factory method creating empty box (constructor should be private)
  * 2. factory method creating box with an element
- * 3. put - puts a new element into the box, throws IllegalStateException if it's empty
+ * 3. put - puts a new element into the box, throws IllegalStateException if it's not empty
  * 4. empty - returns an element in the box and makes it empty, throws IllegalStateException if the box is empty
  * 5. isEmpty - returns true if a box is empty
  *
